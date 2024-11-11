32e5204b95cc2a9092b6b8e91f7ff800a15e9e2c
Atique Khan <atique.khan@zeza.tech>
2024-11-11 15:18:05 +0530

    d

--- funtion/src/main.js
+++ funtion/src/main.js
@@ -7,7 +7,7 @@ export default async ({ req, res, log, error }) => {
   const client = new Client()
     .setEndpoint(process.env.APPWRITE_FUNCTION_API_ENDPOINT)
     .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
-    .setKey(req.headers['x-appwrite-key'] ?? '');
+    .setKey(req.headers['x-appwrite-key'] ?? 'standard_a3f97cc2ff9a273cef1940cce2ca0086115a03c46a3cea19de2eebf83ada4186125b71125e10ce8ac853e2834bce01ff380124f3b380ba8bf4e7d070e04a1889e855dc50e45fe5e2d02a18b4140fffbf2574ba4a27b9450e83d4306495570d5a17bfdeb646f7d4100255e08837ac12b744f0ac439b5a2b9111d51b6bba56ac41');
   const users = new Users(client);
 
   try {
@@ -26,7 +26,10 @@ export default async ({ req, res, log, error }) => {
     return res.text("Pong");
   }
 
+  const response = await users.list();
+
   return res.json({
+    users: response,
     motto: "Build like a team of hundreds_",
     learn: "https://appwrite.io/docs",
     connect: "https://appwrite.io/discord",
