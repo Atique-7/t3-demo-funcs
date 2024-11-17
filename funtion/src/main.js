25001f0db606a393b66e5874f0bb6e8eecaeabb6
Atique Khan <atique.khan@zeza.tech>
2024-11-18 02:46:00 +0530

    fixes

--- funtion/src/main.js
+++ funtion/src/main.js
@@ -7,29 +7,31 @@ export default async ({ req, res, log, error }) => {
   const client = new Client()
     .setEndpoint(process.env.APPWRITE_FUNCTION_API_ENDPOINT)
     .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
-    .setKey(req.headers['x-appwrite-key'] ?? '');
-  const users = new Users(client);
+    .setKey(req.headers['x-appwrite-key'] ?? 'standard_a3f97cc2ff9a273cef1940cce2ca0086115a03c46a3cea19de2eebf83ada4186125b71125e10ce8ac853e2834bce01ff380124f3b380ba8bf4e7d070e04a1889e855dc50e45fe5e2d02a18b4140fffbf2574ba4a27b9450e83d4306495570d5a17bfdeb646f7d4100255e08837ac12b744f0ac439b5a2b9111d51b6bba56ac41');
+    const users = new Users(client);
 
-  try {
+    try {
+      const response = await users.list();
+      // Log messages and errors to the Appwrite Console
+      // These logs won't be seen by your end users
+      log(`Total users: ${response.total}`);
+    } catch(err) {
+      error("Could not list users: " + err.message);
+    }
+  
+    // The req object contains the request data
+    if (req.path === "/ping") {
+      // Use res object to respond with text(), json(), or binary()
+      // Don't forget to return a response!
+      return res.text("Pong");
+    }
+  
     const response = await users.list();
-    // Log messages and errors to the Appwrite Console
-    // These logs won't be seen by your end users
-    log(`Total users: ${response.total}`);
-  } catch(err) {
-    error("Could not list users: " + err.message);
-  }
-
-  // The req object contains the request data
-  if (req.path === "/ping") {
-    // Use res object to respond with text(), json(), or binary()
-    // Don't forget to return a response!
-    return res.text("Pong");
-  }
-
-  return res.json({
-    motto: "Build like a team of hundreds_",
-    learn: "https://appwrite.io/docs",
-    connect: "https://appwrite.io/discord",
-    getInspired: "https://builtwith.appwrite.io",
-  });
-};
+  
+    return res.json({
+      users: response
+    });
+    // return res.json({
+    //   users: response
+    // });
+  };
